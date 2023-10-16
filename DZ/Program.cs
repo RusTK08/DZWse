b987b8e0e45d3386c4835e1f33aea94fc4370354
Ruslan Romanov <ruslan97_1997@mail.ru>
2023-10-16 15:58:41 +0300

    Первое задание

---
+++ DZ/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введите число!");
+string numberOne = Console.ReadLine();
+int gotov = Convert.ToInt32(numberOne);
+Console.WriteLine("Введите число 2!");
+string numberTwo = Console.ReadLine();
+int gotov2 = Convert.ToInt32(numberTwo);
+int max = gotov;
+int min = gotov2;
+if(gotov == gotov2) 
+{
+    Console.WriteLine("Введите число не равное другому"); 
+}
+else if(gotov > gotov2) 
+{
+    max = gotov; min = gotov2;
+    Console.Write("Максимальное ");
+    Console.WriteLine(max );
+    Console.Write("Минимальное ");
+    Console.WriteLine(min );
+}
+else
+{
+    max = gotov2; min = gotov;
+    Console.Write("Максимальное ");
+    Console.WriteLine(max );
+    Console.Write("Минимальное ");
+    Console.WriteLine(min );
+}
