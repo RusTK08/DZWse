e0952d038c185a486b3b2c232b698a3523f4e6b5
Ruslan Romanov <ruslan97_1997@mail.ru>
2023-10-16 21:00:47 +0300

    Четвёртое задание

---
+++ DZ4/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число");
+string num1 = Console.ReadLine();
+int got1 = Convert.ToInt32(num1);
+for(int N = got1; N <= got1; N++)
+{
+    Console.Write(got1);
+}
+// int array = {1, got1}; 
+// int index0 = 1;
+// int size = got1;
+// int currentidex = index0;
+// string otvet2 = otvet, + "" +;
+// while(array[currentindex] < got1)
+// {
+//     if(currentidex == index[size - 1])
+//     {
+//         Console.WriteLine(otvet2);
+//     }
+//     currentidex++;
+//     string otvet = array[currentidex++];
+// }
