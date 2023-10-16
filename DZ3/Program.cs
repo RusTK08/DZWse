532f46f3ef14e808c42205a764f320dac3b17693
Ruslan Romanov <ruslan97_1997@mail.ru>
2023-10-16 17:58:34 +0300

    Третье задание

---
+++ DZ3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число!");
+string numberA = Console.ReadLine();
+int got1 = Convert.ToInt32(numberA);
+int got2 = 2;
+int result = got1 % got2;
+if(result == 0)
+{
+    Console.WriteLine("Число чётное");
+}
+else
+{
+    Console.WriteLine("Число нечётное");
+}
