86ce069d9313727c2a28c229e1e2ef0b0f4209ca
Ruslan Romanov <ruslan97_1997@mail.ru>
2023-10-17 00:27:12 +0300

    Начал пятое

---
+++ DZ5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число!");
+string num1 = Console.ReadLine();
+int exe = Convert.ToInt32(num1);
+string num2 = Convert.ToString(exe);
+
+// n + b + c = num2;
+Console.WriteLine(num2);
+// Console.WriteLine("Введите число!");
+// string num1 = Console.ReadLine();
+// int exe = Convert.ToInt32(num1);
+// int array{} = exe;
+// Console.WriteLine(array);
