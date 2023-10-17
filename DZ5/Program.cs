d8f5db301ab70223e7b1955e2652417ebbbabd93
Ruslan Romanov <ruslan97_1997@mail.ru>
2023-10-17 10:55:43 +0300

    Пятое готово

--- DZ5/Program.cs
+++ DZ5/Program.cs
@@ -1,10 +1,16 @@
 ﻿Console.WriteLine("Введите число!");
 string num1 = Console.ReadLine();
 int exe = Convert.ToInt32(num1);
-string num2 = Convert.ToString(exe);
-
-// n + b + c = num2;
-Console.WriteLine(num2);
+if(exe >99 && exe <1000)
+{
+    int result = exe % 100;
+    int result2 = result / 10;
+    Console.WriteLine(result2);
+}
+else
+{
+    Console.WriteLine("Введите число больше 99 и меньше 1000");
+}
 // Console.WriteLine("Введите число!");
 // string num1 = Console.ReadLine();
 // int exe = Convert.ToInt32(num1);
